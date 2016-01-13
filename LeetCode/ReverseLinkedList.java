12decd0508ec272e23d8c05c111088749bce0cf8
Ahmed Elbagoury <am.elbagoury@gmail.com>
2016-01-13 11:56:31 -0500

    Update ReverseLinkedList.java
    Add iterative implementation

--- LeetCode/ReverseLinkedList.java
+++ LeetCode/ReverseLinkedList.java
@@ -7,6 +7,27 @@
  * }
  */
 public class Solution {
+     public ListNode reverseListIterative(ListNode head) {
+         if(head == null || head.next == null)
+            return head;
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+        ListNode newHead = p2;
+        ListNode nextNext = p2.next;
+        ListNode prev = null;
+        while(p2 != null){
+            p1.next = prev;
+            p2.next = p1;
+            prev = p1;
+            p1 = p2;
+            newHead = p2;
+            p2 = nextNext;
+            if(nextNext != null)
+                nextNext = nextNext.next;
+        }
+        return newHead;
+    }
+    
      public ListNode reverseList(ListNode head) {
         if(head == null)
             return head;
