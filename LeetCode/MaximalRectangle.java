0f204d08089a31fd2f2b6a4229c1f04cd412e778
Ahmed Elbagoury <am.elbagoury@gmail.com>
2016-02-11 12:30:01 -0500

    Update MaximalRectangle.java

--- LeetCode/MaximalRectangle.java
+++ LeetCode/MaximalRectangle.java
@@ -23,6 +23,19 @@ public class MaximalRectangle{
     }
     
     public int maxRectangle(int[] height) {
+    	/*
+    		http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+    		 For every bar ‘x’, we calculate the area with ‘x’ as the smallest bar in the rectangle. 
+    		 If we calculate such area for every bar ‘x’ and find the maximum of all areas, our task is done. 
+    		 How to calculate area with ‘x’ as smallest bar? 
+    		 We need to know index of the first smaller (smaller than ‘x’) bar on left of ‘x’ and index of first smaller bar on right of ‘x’. 
+    		 Let us call these indexes as ‘left index’ and ‘right index’ respectively.
+		We traverse all bars from left to right, maintain a stack of bars. 
+		Every bar is pushed to stack once. A bar is popped from stack when a bar of smaller height is seen. 
+		When a bar is popped, we calculate the area with the popped bar as smallest bar. 
+		How do we get left and right indexes of the popped bar – the current index tells us the ‘right index’ and 
+		index of previous item in stack is the ‘left index’.
+    	*/
         if(height == null)
             return 0;
         int n = height.length;    
