1c0a82dc002cac6e374ba187f19f7168efdf6f02
Ahmed Elbagoury <am.elbagoury@gmail.com>
2016-09-04 03:56:30 -0300

    Update FlattenBinaryTree.java

--- LeetCode/FlattenBinaryTree.java
+++ LeetCode/FlattenBinaryTree.java
@@ -1,28 +1,49 @@
 //https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 public class FlattenBinaryTree{
 	public void flatten(TreeNode root) {
-        if(root == null)
-            return;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.add(root);
-        TreeNode listHead = null;
-        TreeNode ptr = null;
-        while(!stack.isEmpty()){
-            TreeNode curNode= stack.pop();
-            if(listHead == null){
-                listHead = curNode;
-                ptr = listHead;
-            }
-            else{
-                ptr.right = curNode;
-                ptr = ptr.right;
-            }
-            if(curNode.right != null)
-                stack.push(curNode.right);
-            if(curNode.left != null)    
-                stack.push(curNode.left);
-            curNode.right = null;
-            curNode.left = null;
-        }
-    }
+	        if(root == null)
+	            return;
+	        Stack<TreeNode> stack = new Stack<TreeNode>();
+	        stack.add(root);
+	        TreeNode listHead = null;
+	        TreeNode ptr = null;
+	        while(!stack.isEmpty()){
+	            TreeNode curNode= stack.pop();
+	            if(listHead == null){
+	                listHead = curNode;
+	                ptr = listHead;
+	            }
+	            else{
+	                ptr.right = curNode;
+	                ptr = ptr.right;
+	            }
+	            if(curNode.right != null)
+	                stack.push(curNode.right);
+	            if(curNode.left != null)    
+	                stack.push(curNode.left);
+	            curNode.right = null;
+	            curNode.left = null;
+	        }
+    	}
+    	
+    	public void flatten(TreeNode root) {
+	        if(root == null)
+	            return;
+	        Stack<TreeNode> stack = new Stack<TreeNode>();
+	        stack.push(root);
+	        while(!stack.isEmpty()){
+	        	TreeNode curNode = stack.pop();
+	            if(curNode.right != null){
+	                stack.push(curNode.right);
+	                curNode.right = null;
+	            }
+	            if(curNode.left != null){
+	                stack.push(curNode.left);
+	                curNode.left = null;
+	            }
+	            if(!stack.isEmpty()){
+	            	curNode.right = stack.peek();
+	            }
+	        }
+    	}
 }
