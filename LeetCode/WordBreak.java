30a2b69f0bcd9db498ce4834b343f1ec1976bc3c
Ahmed Elbagoury <am.elbagoury@gmail.com>
2016-09-19 21:05:15 -0300

    Update WordBreak.java

--- LeetCode/WordBreak.java
+++ LeetCode/WordBreak.java
@@ -22,5 +22,25 @@ public class WordBreak{
         }
         return false;
     }
+    
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        if(wordDict == null || wordDict.size() == 0){
+            return false;
+        }
+        boolean [] memo = new boolean[s.length()];
+        int n = s.length();
+        for(int i = n-1; i >= 0 && !memo[i]; i--){
+            for(int j = i; j < n; j++){
+                if(wordDict.contains(s.substring(i, j+1)) && (j == n-1 || memo[j+1])){
+                    memo[i] = true;
+                    if(i == 0){
+                        return true;
+                    }
+                    break;
+                }
+            }
+        }
+        return false;
+    }
 
 }
